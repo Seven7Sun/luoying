f8fe2073147e5ade29b12c6960931aea8dd2ef6e
SevenSun <14036136+sevensun7@user.noreply.gitee.com>
2024-05-14 10:16:49 +0800

    '初次提交'

---
+++ pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="content">
+		<image src="../../static/index/sakuraPicture.png" mode="heightFix"></image>
+		<image src="../../static/index/schoolIcon.png" mode="heightFix" class="school"></image>
+		<text>珞珈山上，樱花树下</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				search: '',
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.school{
+		margin-top: 50rpx;
+		height: 200rpx;
+	}
+
+</style>
