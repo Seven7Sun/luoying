e87eca3c23c4b4cd85ba2f15a38d2cf29eaead29
SevenSun <14036136+sevensun7@user.noreply.gitee.com>
2024-05-14 11:47:53 +0800

    '20240514UPDATE'

---
+++ pages/denglu/denglu.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="content">
+		<view class="title">
+			欢迎来到 珞樱
+
+				
+
+		</view>
+		<view class="verifyBox">
+			<image src="../../static/denglu/verify.png" mode="heightFix" class="icon"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	
+	.title{
+		font-size: 60rpx;
+		font-family: 'siyuanheiti';
+		margin-top: 200rpx;
+	}
+	
+	.verifyBox{
+		margin-top: 200rpx;
+		background-color:rgb(245, 245, 245);
+		width: 500rpx;
+		height: 80rpx;
+		border-radius: 80rpx;
+		display: flex;
+		flex-direction: column;
+		/* align-items: center; */
+		justify-content: center;
+	}
+	
+	.icon{
+		height: 50rpx;
+		margin-left: 20rpx;
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -34,7 +34,9 @@
 		},
 		methods: {
 			tiaozhuan(){
-				
+				uni.navigateTo({
+					url:'/pages/denglu/denglu'
+				})
 			}
 		}
 	}
